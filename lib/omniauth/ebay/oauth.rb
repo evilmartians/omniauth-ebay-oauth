b5c370f1ec1fc49b3ed9299fb45d8700b302d40f
Andrey <paderinandrey2011@gmail.com>
2017-10-25 17:21:04 +0000

    Added gem skeleton

---
+++ lib/omniauth/ebay/oauth.rb
@@ -0,0 +1,9 @@
+require "omniauth/ebay/oauth/version"
+
+module Omniauth
+  module Ebay
+    module Oauth
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/omniauth/ebay/oauth/version.rb
@@ -0,0 +1,7 @@
+module Omniauth
+  module Ebay
+    module Oauth
+      VERSION = "0.1.0"
+    end
+  end
+end
