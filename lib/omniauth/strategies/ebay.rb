c38ae1d45d1db800b94de69f00e61ad7a9da56fb
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-11-03 09:54:23 +0300

    Configure first two OAuth steps

--- lib/omniauth/strategies/ebay.rb
+++ lib/omniauth/strategies/ebay.rb
@@ -7,6 +7,19 @@ module OmniAuth
     # OmniAuth strategy for eBay
     class Ebay < OmniAuth::Strategies::OAuth2
       option :name, :ebay
+
+      option :callback_url
+
+      option :authorize_options, %i[scope]
+      option :client_options,
+             auth_scheme: :basic_auth,
+             user_info_endpoint: 'https://api.ebay.com/ws/api.dll',
+             token_url: 'https://api.ebay.com/identity/v1/oauth2/token',
+             authorize_url: 'https://signin.ebay.com/authorize'
+
+      def callback_url
+        options.callback_url
+      end
     end
   end
 end
