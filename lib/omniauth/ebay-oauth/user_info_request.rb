9528ad7d4a96a1cd76c803a7f0e9cb4d22c82ddf
Ignat Zakrevsky <iezakrevsky@gmail.com>
2017-11-07 04:33:15 +0300

    Remove VERIFY_NONE ssl mode

--- lib/omniauth/ebay-oauth/user_info_request.rb
+++ lib/omniauth/ebay-oauth/user_info_request.rb
@@ -52,7 +52,6 @@ module OmniAuth
         Net::HTTP.new(@url.host, @url.port).tap do |http|
           http.read_timeout = @read_timeout
           http.use_ssl = true
-          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         end
       end
 
